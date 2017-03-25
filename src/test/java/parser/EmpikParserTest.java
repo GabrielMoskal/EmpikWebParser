c92fd56b4bcfe9ed28a1b11aecd50c80541a526e
Gabriel <gab.mos92@gmail.com>
2017-03-25 23:16:42 +0100

    Tests updated

--- src/test/java/parser/EmpikParserTest.java
+++ src/test/java/parser/EmpikParserTest.java
@@ -16,21 +16,18 @@ public class EmpikParserTest {
     public void parseBookInfoTest() {
         List<Pair<String, String>> expectedResult = new ArrayList<Pair<String, String>>();
         expectedResult.add(new Pair<String, String>("Tytuł:", "W kręgach władzy. Tom 1. Wotum nieufności"));
-        expectedResult.add(new Pair<String, String>("Seria:", "W kręgach władzy"));
-        expectedResult.add(new Pair<String, String>("Autor:", "Mróz Remigiusz"));
+        expectedResult.add(new Pair<String, String>("Autor:", "Mróz Remigiusz"));
         expectedResult.add(new Pair<String, String>("Wydawnictwo:", "Filia"));
-        expectedResult.add(new Pair<String, String>("Język wydania:", "polski"));
-        expectedResult.add(new Pair<String, String>("Język oryginału:", "polski"));
-        expectedResult.add(new Pair<String, String>("Ilość stron:", "473"));
-        expectedResult.add(new Pair<String, String>("Data premiery:", "2017-01-11"));
         expectedResult.add(new Pair<String, String>("Rok wydania:", "2017"));
+        expectedResult.add(new Pair<String, String>("Język wydania:", "polski"));
+        expectedResult.add(new Pair<String, String>("Ilość stron:", "473"));
         expectedResult.add(new Pair<String, String>("Format:", "MOBI UWAGA! Ebook chroniony przez watermark. więcej ›"));
         expectedResult.add(new Pair<String, String>("Liczba urządzeń:", "bez ograniczeń"));
         expectedResult.add(new Pair<String, String>("Drukowanie:", "bez ograniczeń"));
         expectedResult.add(new Pair<String, String>("Kopiowanie:", "bez ograniczeń"));
         expectedResult.add(new Pair<String, String>("Indeks:", "20837181"));
 
-        String url = "http://www.empik.com/w-kregach-wladzy-tom-1-wotum-nieufnosci-mroz-remigiusz,p1135880608,ebooki-i-mp3-p";
+        String url = "http://m.empik.com/w-kregach-wladzy-tom-1-wotum-nieufnosci-mroz-remigiusz,p1135880608,ebooki-i-mp3-p";
         List<Pair<String, String>> result = null;
         try {
             EmpikParser parser = new EmpikParser(url);
@@ -47,7 +44,7 @@ public class EmpikParserTest {
                 "Marszałek sejmu, Daria Seyda, budzi się w pokoju hotelowym, nie pamiętając, jak się w nim znalazła ani co się z nią działo przez ostatnich dziesięć godzin. Jest przekonana, że stała się ofiarą manipulacji, ale nie wie, kto ani dlaczego może za nią stać. Tymczasem Patryk Hauer, wschodząca gwiazda prawicy, podczas prac komisji śledczej odkrywa polityczny spisek sięgający najistotniejszych osób w kręgach władzy. \n\n" +
                 "Seyda i Hauer znajdują się po przeciwnych stronach sceny politycznej. Dzieli ich wszystko, ale połączy jedna sprawa…";
 
-        String url = "http://www.empik.com/w-kregach-wladzy-tom-1-wotum-nieufnosci-mroz-remigiusz,p1135880608,ebooki-i-mp3-p";
+        String url = "http://m.empik.com/w-kregach-wladzy-tom-1-wotum-nieufnosci-mroz-remigiusz,p1135880608,ebooki-i-mp3-p";
         String result = null;
         try {
             EmpikParser parser = new EmpikParser(url);
@@ -61,8 +58,8 @@ public class EmpikParserTest {
     @Test
     public void parseLinksToConcreteBookUrls() throws IOException {
         List<String> expectedResult = new ArrayList<String>();
-        expectedResult.add("http://www.empik.com/portret-modej-wenecjanki-pilch-jerzy,p1136197781,ebooki-i-mp3-p");
-        expectedResult.add("http://www.empik.com/wielkie-klamstewka-moriarty-liane,p1137958541,ebooki-i-mp3-p");
+        expectedResult.add("http://m.empik.com/inwigilacja-mroz-remigiusz,p1137827825,ebooki-i-mp3-p");
+        expectedResult.add("http://m.empik.com/kasacja-mroz-remigiusz,p1106211525,ebooki-i-mp3-p");
 
         String filePath = new File("").getAbsolutePath();
         filePath = filePath + "\\src\\test\\java\\parser\\PageWithLinksToConcreteEbooks.html";
